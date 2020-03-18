baf687492cc45ff821ede2ca22d8081b7b727307
will zheng <will.zheng@unisco.com>
2020-03-18 17:25:30 +0800

    init

---
+++ src/main/java/com/shop/htjy/HtjyApplication.java
@@ -0,0 +1,13 @@
+package com.shop.htjy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtjyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HtjyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/htjy/HtjyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.htjy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtjyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
